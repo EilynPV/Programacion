be735e265f86c672d0d99c3200fd4eec55dea96b
EilynPV <143530990+EilynPV@users.noreply.github.com>
2023-09-05 10:07:06 -0600

    python

---
+++ Laboratorio 1 Eilyn Parada.py
@@ -0,0 +1,19 @@
+#print("Hola Mundo Soy Eilyn")
+
+'''print("Hola Mundo", end="")
+print("Soy Eilyn", end="")'''
+
+#print nos sirve para mostrar un mensaje que se enccuentra en una misma linea, mientras que el printend nos une mensajes que esten en diferentes lineas quitando el salto de linea entre ellas#
+
+'''texto =input()
+print(texto + "Hola")'''
+
+print("Ingrese su nombre:")
+Nombre= input()
+
+print("Hola Mundo")
+print( "soy " + Nombre)
+
+
+print("Hola Mundo", end="")
+print( "soy " + Nombre, end="")
